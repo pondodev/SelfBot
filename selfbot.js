fa1facceff4d415d98515c6256eadfc6e09f80da
Daniel Coady <daniel.coady@outlook.com.au>
2017-05-12 17:43:44 +1000

    Adjusted formatting and changed commands
    Removed !say and made the default case for switch statement just break
    to avoid unecessary spam in chat

--- selfbot.js
+++ selfbot.js
@@ -2,56 +2,60 @@ const Discord = require("discord.js");
 const config = require("./config.json");
 const bot = new Discord.Client();
 
-function construct(fromMessage) {
+function construct(fromMessage)
+{
     var temp = "";
-    for (var i = 1; i < messageTemp.length; i++) {
-        temp += " " + messageTemp[i];
+    for (var i = 1; i < fromMessage.length; i++)
+    {
+        temp += " " + fromMessage[i];
     }
     return temp;
 }
 
-bot.on("ready", () => {
+bot.on("ready", () =>
+{
     console.log("Junkrat primed and ready!");
 });
 
-bot.on("message", message => {
-    if (message.author !== bot.user) return;
+bot.on("message", message =>
+{
+    if (message.author !== bot.user) return; // Check command is coming from ourself
 
     var contents = message.content.split(" ");
 
-    if (!contents[0].startsWith("!")) {
-        return;
-    }
-    
+    if (!contents[0].startsWith("!")) return; // If the message isn't a command just skip it
+
     var commandName = contents[0].substring(1);
-    
-    switch(commandName) {
-        case "quoteme": // Quotes a message said by this user
-            message.delete();
-            message.channel.send("", {
+
+    switch(commandName)
+    {
+    case "quoteme": // Quotes a message said by this user
+        message.delete();
+        message.channel.send("",
+            {
                 embed: {
                     color: 0x00ff00,
-                    author: {
+                    author:
+                    {
                         name: message.author.username,
                         icon_url: message.author.avatarURL
                     },
                     description: construct(contents),
-                    footer: {
+                    footer:
+                    {
                         icon_url: bot.user.avatarURL,
                         text: "Brought to you by p0isonedtechtips™"
                     }
                 }
             });
-            
-            break;
-        
-        case "say": // A testing message
-            message.channel.sendMessage("Hello, world!");
-            break;
-            
-        case "flipcoin": // Flips a coin
-            if (Math.floor(Math.random() * (2 + 1)) == 1) {
-                message.channel.send("", {
+
+        break;
+
+    case "flipcoin": // Flips a coin
+        if (Math.floor(Math.random() * (2 + 1)) == 1)
+        {
+            message.channel.send("",
+                {
                     embed: {
                         color: 0xFF0000,
                         description: "Heads!",
@@ -62,8 +66,11 @@ bot.on("message", message => {
                     }
                 });
 
-            } else {
-                message.channel.send("", {
+        }
+        else
+        {
+            message.channel.send("",
+                {
                     embed: {
                         color: 0x0000FF,
                         description: "Tails!",
@@ -73,12 +80,12 @@ bot.on("message", message => {
                         }
                     }
                 });
-            }
-            
-            break;
-            
-        default:
-            message.channel.sendMessage("Unknown command!");
+        }
+
+        break;
+
+    default: // If we don't recognise the command, just break
+        break;
     }
 });
 
